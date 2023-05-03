430adb00aaeceb62345689a1c45cf177e511209c
Shokhzod2202 <61985019+Shokhzod2202@users.noreply.github.com>
2023-05-04 07:28:53 +0900

    Innitial Commit

---
+++ ViewbindingRV/app/src/main/java/com/example/viewbindingrv/TaskList.kt
@@ -0,0 +1,17 @@
+package com.example.viewbindingrv
+
+object TaskList {
+    val taskListv = listOf<Task>(
+        Task("Take a Walk", timeStamp = "7:00 am"),
+        Task("Eat Breakfast", timeStamp = "8:00 am"),
+        Task("Attend a Meeting", timeStamp = "9:00 am"),
+        Task("Read a Book", timeStamp = "12:00 pm"),
+        Task("Eat a Lunch", timeStamp = "2:00 pm"),
+        Task("Buy Groceries", timeStamp = "4:00 pm"),
+        Task("Watch YouTube Videos", timeStamp = "6:00 pm"),
+        Task("Eat Dinner", timeStamp = "7:00 pm"),
+        Task("Go to Bed", timeStamp = "8:00 pm")
+
+
+    )
+}
