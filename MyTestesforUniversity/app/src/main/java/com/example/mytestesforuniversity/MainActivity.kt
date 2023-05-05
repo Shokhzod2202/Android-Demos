1e55efd1e490ddf42a4e2797936c2a5e0027c495
Shokhzod2202 <61985019+Shokhzod2202@users.noreply.github.com>
2023-05-06 08:02:30 +0900

    Debugging MyTestForUniversity project

--- MyTestesforUniversity/app/src/main/java/com/example/mytestesforuniversity/MainActivity.kt
+++ MyTestesforUniversity/app/src/main/java/com/example/mytestesforuniversity/MainActivity.kt
@@ -11,6 +11,7 @@ class MainActivity : AppCompatActivity() {
     var myInput : TextView? = null
     var myOutput : TextView? = null
     var btnCalc : Button? = null
+    var AddInfos : TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,13 +20,40 @@ class MainActivity : AppCompatActivity() {
         myInput = findViewById(R.id.etInput)
         myOutput = findViewById(R.id.tvOutput)
         btnCalc = findViewById(R.id.btnCalculate)
+        AddInfos = findViewById(R.id.tvAddInfo)
 
         myInput?.text = myArray.toString()
 
         btnCalc?.setOnClickListener{
             //TODO -> enable entered text to be calculated.
-//            var newInput: ArrayList<Int> = myInput?.text?
-            myArray.sort()
+            var inputText = myInput?.text.toString()
+            val values = inputText.replace("[","").replace("]","").split(",")
+            if(myArray!=null){ myArray.clear()}
+            for(value in values){
+                val intValue = value.trim().toIntOrNull()
+                if(intValue != null){
+                    myArray.add(intValue)
+                }
+            }
+            var addInfo = "Info "
+
+            // You can see it -> for(i in (myArray.size - 1))
+            for (i in 1 until myArray.size){
+                addInfo += "myArray -> $myArray \n"
+                val key = myArray[i]
+                var j = i-1
+                addInfo += "i = ${myArray[i]} | key = $key | j = ${myArray[j]} \n "
+                while (j >= 0 && myArray[j] > key){
+                    myArray[j+1] = myArray[j]
+                    j--
+                    addInfo += "myArray -> $myArray \n"
+                }
+                myArray[j+1] = key
+                addInfo += "myArray[${j+1}] = key  ($key)\n\n"
+            }
+            addInfo += "myArray -> $myArray \n"
+
+            AddInfos?.text = addInfo
             myOutput?.text = myArray.toString()
         }
 
